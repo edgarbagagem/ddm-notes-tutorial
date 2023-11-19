4c2ea9fedbb242ef93375da22341cdf255e71377
edgar bagagem <edgarbagagem@gmail.com>
2023-11-19 21:14:17 +0000

    Notes app#

---
+++ notes/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace notes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+
+        }
+    }
+}
---
+++ notes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace notes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
